c38cd74532e2e558fcae4a198fdec27785f0c61a
AwooDancer <f158119560@gmail.com>
2023-06-05 18:01:58 +0800

    绘制单变量、多变量时序数据图

--- demo_kats/demo_kats_basic.py
+++ demo_kats/demo_kats_basic.py
@@ -12,10 +12,19 @@ warnings.filterwarnings('ignore')
 # 导入单变量数据，一定要包含时间列“time”， 或者给原始数据的列重命名为“time”，
 air_passengers_df = pd.read_csv(r"D:\Downloads\BrowserDownloads\kats-0.2.0\kats\data\\air_passengers.csv")
 
-air_passengers_df.columns = ['time', 'value']
+air_passengers_df.columns = ["time", "value"]
 
 air_passengers_df.plot()
 print(air_passengers_df.head(3))
 print(air_passengers_df.describe())
+plt.xlabel("time")
+plt.show()
 
+# 导入多变量数据
+multi_ts_df = pd.read_csv(r"D:\Downloads\BrowserDownloads\kats-0.2.0\kats\data\multi_ts.csv", index_col=0)
+multi_ts_df.columns = ["time", "v1", "v2"]
+multi_ts_df.head()
+# 画在相同时间下的多变量时序图
+multi_ts_df.plot()
+plt.xlabel("time")
 plt.show()
